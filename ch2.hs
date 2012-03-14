66406cffda58ad37df3f7ccaf4144ad7ff45e254
Nick Orton <orton.nick@gmail.com>
2012-03-13 21:29:53 -0400

    questions from chapter 2

--- ch2.hs
+++ ch2.hs
@@ -1,3 +1,8 @@
+-- 1. What are the types of the following Expressions?
+-- False :: Bool
+-- (["foo", "bar"], 'a') :: ([[Char]], Char)
+-- [(True, []), (False, [['a']])] :: [(Bool, [[Char]])
+
 -- 1.  What are valid behaviors of type [a] -> a
 --   Retrieve the first element of the list.
 --   Retrieve the last element of the list
@@ -9,3 +14,6 @@ lastButOne :: [a] -> a
 lastButOne (x:_:[]) = x
 lastButOne (_:xs) = lastButOne xs
 
+-- 3. What happens when you pass it a list that's too short?
+-- *Main> lastButOne [3]
+-- *** Exception: ch2.hs:(9,1)-(10,33): Non-exhaustive patterns in function lastButOne]
