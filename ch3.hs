df49905b8e55597409d0687ffb325be7cd2be3e7
Nick Orton <orton.nick@gmail.com>
2012-03-14 22:52:52 -0400

    some chpt 3 problems

--- ch3.hs
+++ ch3.hs
@@ -1,30 +1,56 @@
+import Data.List
+import Data.Ord
+
 -- 1.  Write a function that computes the number of elements in a list. To 
 --     test it, ensure that it gives the same answers as the standard length 
 --     function.
-
 -- 2.  Add a type signature for your function to your source file. To test it,
 --     load the source file into ghci again. 
+
+
+myLength :: [a] -> Integer
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
 
 -- 3.  Write a function that computes the mean of a list, i.e. the sum of all 
 --     elements in the list divided by its length. (You may need to use the
 --     fromIntegral function to convert the length of the list from an integer
 --     into a floating point number.) 
 
+mySum :: [Int] -> Int
+mySum [] = 0
+mySum (x:xs) = x + mySum xs
+
+mean :: Fractional a => [Int] -> a
+mean list = fromIntegral (mySum list) / fromIntegral (length list)
+
 -- 4.  Turn a list into a palindrome, i.e. it should read the same both
 --     backwards and forwards. For example, given the list [1,2,3], your 
 --     function should return [1,2,3,3,2,1]. 
 
+consReverse list = list ++ reverse list
+
 -- 5.  Write a function that determines whether its input list is a palindrome.
+
+isPalendrome list = list == reverse list
 
 -- 6.  Create a function that sorts a list of lists based on the length of each
 --     sublist. (You may want to look at the sortBy function from the Data.List
 --     module.) 
+
+compareLengths l1 l2
+  | length l1 < length l2 = LT
+  | length l1 > length l2 = GT
+  | otherwise = EQ
+
+sortLists listOfLists = sortBy compareLengths listOfLists
 
 -- 7.  Define a function that joins a list of lists together using a separator
 --     value. 
 
 intersperse :: a -> [[a]] -> [a]
 intersperse _ _ = error "not yet implemented"
+
 --     ghci> :load Intersperse
 --     [1 of 1] Compiling Main             ( Intersperse.hs, interpreted )
 --     Ok, modules loaded: Main.
